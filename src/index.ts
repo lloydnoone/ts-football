f10e6ff2584051276dea9f38d6227a9b4e040591
lloydnoone <lloydnoone92@gmail.com>
2020-11-19 16:52:35 +0000

    add refactor to interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { MatchReader } from './MatchReader'
 import { MatchResult } from './MatchResult'
+import { CsvFileReader } from './CsvFileReader'
 
-const reader = new MatchReader('football.csv')
-reader.read()
+//create an object that satisfies the datareader interface
+const csvFileReader = new CsvFileReader('football.csv')
+
+//create an instance of matchreader and pass in something satisfying the data reader interface
+const matchReader = new MatchReader(csvFileReader)
+matchReader.load()
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
